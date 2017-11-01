b8d0dc179b39564a23176fde4e56722c7ce879b9
ini106 <inicca_lee@dlsu.edu.ph>
2017-10-31 20:49:55 -0700

    Import Update version

--- src/pokemon/PokemonPC.java
+++ src/pokemon/PokemonPC.java
@@ -346,6 +346,7 @@ public class PokemonPC extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(PokemonPC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
